89d8f840dab5c97f5b181299512cf2f4690e1d61
Abhay Dutt <abhaydutt@Abhays-MacBook-Air.local>
2022-09-28 10:23:49 +0530

    Sort colors on leetcode

---
+++ src/arrays/Sort012SortColors.java
@@ -0,0 +1,37 @@
+package arrays;
+
+public class Sort012SortColors {
+	public static void sortColors(int [] nums) {
+		int nextZero = 0;
+		int nextTwo = nums.length-1;
+		for(int i = 0;i<nums.length && i<=nextTwo;) {
+			if(nums[i] == 1) {
+				i++;
+			}else if(nums[i] == 0) {
+				int temp = nums[nextZero];
+				nums[nextZero] = nums[i];
+				nums[i] = temp;
+				nextZero++;
+				i++;
+			}else if(nums[i] == 2) {
+				int temp = nums[nextTwo];
+				nums[nextTwo] = nums[i];
+				nums[i] = temp;
+				nextTwo--;
+			}
+			for(int a = 0;a<nums.length;a++)
+				System.out.print(nums[a]+" ");
+				
+				System.out.println("");
+		}
+	}
+
+	public static void main(String[] args) {
+		int arr[] = {2,0,2,1,1,0};
+		sortColors(arr);
+//		for(int i = 0;i<arr.length;i++)
+//			System.out.print(arr[i] +" ");
+		
+	}
+
+}
