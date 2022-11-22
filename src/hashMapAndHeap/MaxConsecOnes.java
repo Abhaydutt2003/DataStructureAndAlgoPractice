784bd3c1fdc29b8cacacfca5aa6a99b5109d2347
Abhaydutt2003 <dutt.abhay@gmail.com>
2022-11-22 15:49:36 +0530

    done on leetcode
    very easy, normal Madame algorithm...

---
+++ src/hashMapAndHeap/MaxConsecOnes.java
@@ -0,0 +1,23 @@
+package hashMapAndHeap;
+
+public class MaxConsecOnes {
+
+	public static void main(String[] args) {
+		int arr[] = {1,1,0,1,1,1};
+		System.out.print(findMaxConsecutiveOnes(arr));
+
+	}
+	public static int findMaxConsecutiveOnes(int[] nums) {
+		int maxLen = Integer.MIN_VALUE,current = 0;
+		for(int i = 0;i<nums.length;i++) {
+			if(nums[i] == 0) {
+				current = 0;
+			}else {
+				current++;
+				if(maxLen<current) {maxLen = current;}
+			}
+		}
+		return maxLen;
+	}
+
+}
