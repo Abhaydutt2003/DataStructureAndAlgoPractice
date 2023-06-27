8001e75bf482c7dcd6532e183b3718c5d041af7f
Abhaydutt2003 <dutt.abhay@gmail.com>
2023-06-27 13:17:27 +0530

    Update CheckIfStraightLine.java

--- src/Math/CheckIfStraightLine.java
+++ src/Math/CheckIfStraightLine.java
@@ -19,8 +19,17 @@ public class CheckIfStraightLine {
             int y2 = matrix[i-1][1];
             int y3 = matrix[i][1];
             if(isValid(x1,x2,x3,y1,y2,y3) == false){
-
+                return false;
             }
+        }
+        return true;
+    }
+
+    public static boolean isValid(int x1,int x2, int x3, int y1, int y2, int y3){
+        if(((y2-y1) * (x3-x2)) == ((y3-y2)*(x2-x1))){
+            return true;
+        }else{
+            return false;
         }
     }
 
