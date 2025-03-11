7c437e7ee2b1a2e496264072142593dbf3fa5607
Abhay Dutt <dutt.abhay@gmail.com>
2025-03-11 16:44:46 +0530

    latest commit

--- python-dsa/stacks/nge2.py
+++ python-dsa/stacks/nge2.py
@@ -1,3 +1,16 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        map = {}
+        stack = []
+
+        def traveseNums():
+            for num in nums:
+                while stack and stack[-1] < num:
+                    map[stack.pop()] = num
+                stack.append(num)
         
+        # traveseNums()
+        # if stack:
+        #     traveseNums()
+        return [map.get(num) for num in nums]
+
